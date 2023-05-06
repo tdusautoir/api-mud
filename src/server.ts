943ca0fb769157e66391234685949a62057d9231
Thibaut Dusautoir <tdusautoir@myges.fr>
2023-05-06 19:22:09 +0200

    setup typescript

---
+++ src/server.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import http from 'http';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const port = process.env.SERVER_PORT || 3000;
+const app = express();
+
+/** CONNECT TO DATABASE */
+if (process.env.MONGO_URI) {
+    mongoose
+        .connect(process.env.MONGO_URI)
+        .then(() => {
+            console.log('Connected to database');
+        })
+        .catch((error) => {
+            console.log(error);
+            process.exit(1);
+        });
+} else {
+    console.error('MONGO_URI environment variable is not set.');
+}
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+/** HEALTHCHECK */
+app.get('/healthcheck', (req, res, next) => {
+    res.status(200).json({ message: 'OK' });
+});
+
+http.createServer(app).listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
